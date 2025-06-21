ccc55ce017c79f6a4062c8ab5ad78d7fbb615cef
Dominic Fallows <consultant@dominicfallows.uk>
2025-06-21 10:52:13 +0100

    Update raw_data_resource to use absolute path for JSON file and add raw_data.json to .gitignore

--- shared_code_location/shared/resources.py
+++ shared_code_location/shared/resources.py
@@ -1,11 +1,18 @@
 from dagster import resource
 import json
+import os
+
 
 @resource
 def raw_data_resource(_):
     """Loads example data from a JSON file as a Dagster resource."""
-    with open("example_resources/raw_data.json") as f:
+    # Determine absolute path to raw_data.json
+    base_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), "..", ".."))
+    file_path = os.path.join(base_dir, "example_resources", "raw_data.json")
+    with open(file_path) as f:
         return json.load(f)
+
 
 @resource
 def output_dir_resource(_):
